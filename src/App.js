b1dbcd1fca018066a6165cc05680c749c6d13775
NirajTiwari7 <nirajtiwari0716@gmail.com>
2022-12-05 20:55:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React , { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+function App() {
+ 
+  const [mode, setMode] = useState('light'); // WHEATHER MODE IS ENABLE OR NOT
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message ,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+    // apna aap 2second bad showalert chala jaya ga
+  }
+
+  const toggleMode = ()=>{
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#0d0d0d';
+      showAlert("Dark mode has been enabled" , "success")
+      document.title = 'TextUtils - Dark Mode'; // title change ho jaya ga dark mode enable karna sa
+
+      // == [ JAISA KOI KOI WEBSITE MAI BLINK HOTA RAHA HAI KOI IMAGE YA TEXT KI PHONE MAI VIRUS HAI ETC ] { MAI BHI AISA HI KUCH KARNA CHAHTA PAR MAI TITLE KO BLINK KAR VANA CHAHTA HO  }
+      /*
+      setInterval(() => {
+        document.title = 'TextUtils is Amazing Mode';
+      }, 2000);
+      setInterval(() => {
+        document.title = 'Install TextUtils Now';
+      }, 4000);
+      */
+    }
+     else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled" , "success")
+      document.title = 'TextUtils - Light Mode'; // -- title change ho jaya ga light mode enable karna sa
+
+      
+      // == [ JAISA KOI KOI WEBSITE MAI BLINK HOTA RAHA HAI KOI IMAGE YA TEXT KI PHONE MAI VIRUS HAI ETC ] { MAI BHI AISA HI KUCH KARNA CHAHTA PAR MAI TITLE KO BLINK KAR VANA CHAHTA HO  }
+      /*
+      setInterval(() => {
+        document.title = 'TextUtils is Amazing Mode';
+      }, 2000);
+
+      setInterval(() => {
+        document.title = 'Install TextUtils Now';
+      }, 4000);
+      */
+    }
+    }
+
+  return (
+   <> 
+   
+
+   <BrowserRouter>
+    <Navbar  title="TextUtils"  abouttext="About" mode={mode} toggleMode={toggleMode} />
+
+   <Alert alert={alert} />
+   <div className="container my-3">
+    <Routes>
+      {/** its recommended to use exact  */}
+       <Route eaxct path="/about" element={<About />} />
+       <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
+     </Routes>
+     </div>
+   </BrowserRouter>
+   
+ </>
+  );
+}
+
+export default App;
+
+
+
